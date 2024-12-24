65164edaa66f58830bad9d9bdeb9d43cf6e7e774
Denys Bokov <denisbokov1703@gmail.com>
2024-12-24 17:05:52 +0100

    feat(news): configure Article view in django admin

--- main/news/admin.py
+++ main/news/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Article
 
+
+class ArticleAdmin(admin.ModelAdmin):
+    # to configure how much and which columns we will se in Article admin
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    # with columns will have a link to the article info
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
 # Register your models here.
-admin.site.register(Article)
+
+admin.site.register(Article, ArticleAdmin) # [model, modelAdmin]
