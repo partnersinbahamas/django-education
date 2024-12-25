1b40f52acc72879744addeedaa7b68c0fc3bc08f
Denys Bokov <denisbokov1703@gmail.com>
2024-12-25 14:43:11 +0100

    feat(news): register Category, and new settings for ArticleAdmin

--- main/news/admin.py
+++ main/news/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
 
 
 class ArticleAdmin(admin.ModelAdmin):
     # to configure how much and which columns we will se in Article admin
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published', 'category')
     # with columns will have a link to the article info
     list_display_links = ('id', 'title')
-    search_fields = ('title', 'content')
+    search_fields = ('title', 'content', 'category')
+
+    # which columns can you edit in Articles list
+    list_editable = ('is_published',)
+
+    # which columns can you filter in Articles list
+    list_filter = ('created_at', 'updated_at', 'is_published', 'category')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields=('name',)
 
 # Register your models here.
 
 admin.site.register(Article, ArticleAdmin) # [model, modelAdmin]
+admin.site.register(Category, CategoryAdmin)
