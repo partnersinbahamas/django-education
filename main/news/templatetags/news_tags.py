efd541ab6706c4aae204dacf05d0ddd7517e5d90
Denys Bokov <denisbokov1703@gmail.com>
2024-12-28 19:22:52 +0100

    feat: render categories with inclusion tag

--- main/news/templatetags/news_tags.py
+++ main/news/templatetags/news_tags.py
@@ -5,6 +5,14 @@ from news.models import Category
 register = template.Library()
 
 # you can use name paramatr to change function name
+# Simple tag uses to return values
 @register.simple_tag()
 def get_all_categories():
     return Category.objects.all()
+
+# Inclusion tag uses to return values an render the templates
+@register.inclusion_tag('news/list_categories.html')
+def show_all_categories(selected_category=None):
+    categories = Category.objects.all()
+
+    return {'categories': categories, 'selected_category': selected_category}
