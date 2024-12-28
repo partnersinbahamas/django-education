d4da57126500d15ac0d4baa5e021a6c8a9bef162
Denys Bokov <denisbokov1703@gmail.com>
2024-12-28 16:06:58 +0100

    feat: create categories page, with filtered articles

--- main/news/urls.py
+++ main/news/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index, by_category
 
 urlpatterns = [
-    path('', index, name="news")
+    path('', index, name="news"),
+    # <int:pk> means that we will have dynamic link param
+    path('category/<int:pk>', by_category)
 ]
--- main/news/views.py
+++ main/news/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Article
+from .models import Article, Category
 
 
 # Create your views here.
 
 def index(request):
     news = Article.objects.order_by('-created_at')
+    categories = Category.objects.all()
 
     data = {
       'news': news,
+      'categories': categories,
     }
+    return render(request, 'news/news.html', data)
+
+
+def by_category(request, pk):
+    # pk came from url category/<int:pk> in urls
+    news = Article.objects.filter(category_id=pk)
+    categories = Category.objects.all()
+    selected_category = categories.get(id=pk)
+
+    data = {
+        'news': news,
+        'categories': categories,
+        'selected_category': selected_category,
+    }
+
     return render(request, 'news/news.html', data)
 
