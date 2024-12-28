8b2edc87e26269a3e388659868474216e22cad6d
Denys Bokov <denisbokov1703@gmail.com>
2024-12-28 16:30:29 +0100

    feat(news): add name url for categories

--- main/news/urls.py
+++ main/news/urls.py
@@ -6,5 +6,5 @@ from .views import index, by_category
 urlpatterns = [
     path('', index, name="news"),
     # <int:pk> means that we will have dynamic link param
-    path('category/<int:pk>', by_category)
+    path('category/<int:pk>', by_category, name="category")
 ]
