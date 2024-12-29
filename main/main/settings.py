7a53fcd4dc65e5df3fc3ff75513979d54bb2b190
Denys Bokov <denisbokov1703@gmail.com>
2024-12-29 16:51:20 +0100

    chore: setup django static files

--- main/main/settings.py
+++ main/main/settings.py
@@ -121,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'main/static',
+    # "/var/www/static/", # needs only for production
+
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- main/main/urls.py
+++ main/main/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('news/', include('news.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # uses only in development stage, django will build the path, and give us the media files
 # in production we do not need this, as it will made in production server
